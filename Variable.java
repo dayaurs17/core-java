5a96063aa69f5655d4edd76759666c32b5e605d8
dayaurs17 <dayanandarajeurs.xworkz@gmail.com>
2023-06-20 15:10:55 +0530

    variable

---
+++ Variable.java
@@ -0,0 +1,83 @@
+public class Variable{
+	public static void main(String args[])
+	{
+		byte noOfSideInTri= 3;
+		byte noOfStates = 4;
+		byte noOfVowels = 5;
+		byte noOfStatesInIndia = 30;
+		byte centuryInTermsOfNo = 100;
+		byte noOfMetroLinesInBengaluru = 2;
+		byte noOfPMInIndia = 2;
+		byte tenandhundread = 110;
+		byte noOfAppuMovies = 30;
+		byte noTeethInMouth = 32;
+		
+		short noOfMLAInIndia = 375;
+		short runsByViratInIpl = 6000;
+		short beerPrice = 190;
+		short movieTicket = 300;
+		short priceOfRedmi8 = 18000;
+		short netflixSubscription = 1800;
+		short distanceBetweenMysBen = 120;
+		short studentsInPESCollege = 12000;
+		short noOfDaysInAYear = 365;
+		short peopleInPresentXworkz = 500;
+		
+		int priceOfIphone = 175000;
+		int populationOfKarnataka = 60000000;
+		int iplViewersInJioCinema = 20000000;
+		int croresInNumbers = 10000000;
+		int yashInstaFollowers = 70000000;
+		int cmSalary = 1000000;
+		int vijaySalary = 80000000;
+		int iasAspirants = 90000000;
+		int noOfGoogleEmployees = 80000000;
+		int speedOfLight = 297870765;
+		
+		long speedOflight = 29786578l;
+		long indiaPopulation = 120000000l;
+		long phoneNO = 9972901311l;
+		long kgfMovieCollection = 10000000000l;
+		long dangalMovieCollection = 60000000000l;
+		long karnataka2021Budget = 500000000000l;
+		long ferrariCarPrice = 1200000000l;
+		long salaryOfSundarPichai = 2500000000l;
+		long buccatiPrice = 50000000000l;
+		long mySalary = 900000000000l;
+		
+		float cgpa = 7.98f;
+		float versionJava = 1.2f;
+		float sgpa = 9.2f;
+		float sslcPassPercentageOfState = 96.7f;
+		float roadTaxOfRoyalEnfield = 25445.45f;
+		float twentyFourDivSeven = 3.1438f;
+		float averageOfKohliInOdi = 55.2f;
+		float sin45Angle = 0.5f;
+		float averageOfSachinInOdi = 57.6f;
+		float twoPlusTwoPointFive = 4.5f;
+		
+		double speedOfNormalTrain = 61.7562; 
+		double kgToLbs = 2.205;
+		double kgInMg = 0.0000001;
+		double indiaArea = 3287263.36;
+		double indianLitrecyRateInPercentage = 77.77;
+		double myAccBalance = 0.0025;
+		double mmInKm = 0.0000001;
+		double cmInKm = 0.00001;
+		double oneSquareKilometer = 24712.807787084;
+		double speedOfLightInKMPerHr = 1079252848.8;
+		
+		boolean isAIsAlphabet = true;
+		boolean is3ANumber = true;
+		boolean is8NotANumber = false;
+		boolean isKannadaLanguage = true;
+		boolean isHindiNationlLanguage = false;
+		boolean isXworkzAGoodTrainingCenter = true;
+		boolean tenPlusTwentyIsThirty = true;
+		boolean isTrueOrFalseACondition = true;
+		boolean isUAMovieCertificate = true;
+		boolean isDellAGoodBrand = true;
+		
+		System.out.println(salaryOfSundarPichai);
+	}
+}
