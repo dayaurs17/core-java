ede16a9fbefd122ecda9f163ab6db5a166ebc48d
dayaurs17 <dayanandarajeurs.xworkz@gmail.com>
2023-06-21 23:10:34 +0530

    methods

---
+++ Array.java
@@ -0,0 +1,58 @@
+public class Array{
+public static void main(String args[]){
+int [] studentMarks = {98,99,92,76,55};
+for(int i=0;i<5;i++)
+{
+	System.out.println(studentMarks[i]);
+}
+int [] rollNoOfStudents = {1,2,3,4,5,6,7};
+for(int r=0;r<7;r++)
+{
+	System.out.println(rollNoOfStudents[r]);
+}
+int [] averageCgpa = {9,7,8,7,9};
+	{
+		System.out.println(averageCgpa[3]);
+	}
+char [] vowels = {'a','e','i','o','u'};
+
+		{
+		System.out.println(vowels[4]);
+	}
+int [] studentAttendenceInPercentage = {98,99,92,76,55};
+for(int i=0;i<5;i++)
+{
+	System.out.println(studentAttendenceInPercentage[i]);
+}
+
+int [] totalScores = {600,598,625,555,602};
+for(int index=0;index<totalScores.length;index++)
+
+	{
+		System.out.println(totalScores[index]);
+	}
+
+int noOfStudents[] = new int [3];
+noOfStudents[0] = 35;
+noOfStudents[1] = 40;
+noOfStudents[2] = 50;
+for(int i=0;i<noOfStudents.length;i++)
+{
+	System.out.println(noOfStudents[i]);
+}
+char rainbowColour[]={'v','i','b','g','y','o','r'};
+System.out.println(rainbowColour[4]);
+long popOfStates[] ={1000000,300000000,400000000,80000000,1000000000};
+System.out.println(popOfStates[3]);
+float priceOfPetrolInEachState[] ={105.32f,99.25f,98.44f,95.45f};
+System.out.println(priceOfPetrolInEachState[4]);
+byte noOfTrainingCenteres[] = {35,55,65,98,100};
+System.out.println(noOfTrainingCenteres[3]);
+int[] noOfStates = new int [15];
+char[] sections = new char[10];
+byte[] noOfTerritories = new byte[5];
+long[] areaInSq = new long[155];
+float[] sgpa = new float[3];
+}
+}
+	
---
+++ methods.java
@@ -0,0 +1,58 @@
+public class Methods{
+	public static void main(String args[]){
+		integers();
+		floats();
+		chars();
+		longs();
+		bytes();
+		
+		
+	}
+		
+		
+		
+		
+		
+		public static void integers()
+		{
+			int []marks = {98,97,96,95,94,90};
+			for(int i=0;i<marks.length;i++)
+			{
+				System.out.println(marks[i]);
+			}
+		}
+		public static void floats()
+		{
+			float []average = {98.2f,97.3f,96.4f,95.12f,94.45f,90.98f};
+			for(int i=0;i<average.length;i++)
+			{
+				System.out.println(average[i]);
+			}
+		}
+		public static void chars()
+		{
+			char [] vowels = {'a','e','i','o','u'};
+			for(int i=0;i<vowels.length;i++)
+			{
+				System.out.println(vowels[i]);
+			}
+		}
+		public static void longs()
+		{
+			long [] areaInSqKm = {765489L,98765L,92345L,987621L};
+			for(int i=0;i<areaInSqKm.length;i++)
+			{
+				System.out.println(areaInSqKm[i]);
+			}
+		}
+		public static void bytes()
+		{
+			byte [] tenTables = {10,20,30,40,50,60,70,80,90,100};
+			for(int i=0;i<tenTables.length;i++)
+			{
+				System.out.println(tenTables[i]);
+			}
+		}
+		
+	
+}
