d5335b30c48c90e0a7bed924e6f303ffc224aea2
dayaurs17 <dayanandarajeurs.xworkz@gmail.com>
2023-07-19 11:47:23 +0530

    Parameter-push

---
+++ Parameter1.java
@@ -0,0 +1,152 @@
+public class Parameter1{
+	public static void main(String[] args){
+		
+		boolean returnedresult = isPrimeNumber(91);
+		System.out.println(returnedresult);
+		char returnScore = getGrade(60);
+		System.out.println(returnScore);
+		boolean returnValue = isEven(12);
+		System.out.println(returnValue);
+		int returnNumber = addition(10,5);
+		System.out.println(returnNumber);
+		boolean returnName = checkStudentName("harshi");
+		System.out.println(returnName);
+		long returnNumber2 = getLargeNumber(40,20);
+		System.out.println(returnNumber2);
+		short returnValue2 = isPositive(30);
+		System.out.println(returnValue2);
+		short returnValue3 = isPositive(-20);
+		System.out.println(returnValue3);
+		long returnNumber3 = getSmallNumber(50,20);
+		System.out.println(returnNumber3);
+		
+		
+		
+	}
+    public static boolean isPrimeNumber(int number)
+	{	
+		if(number%2==0)
+		{
+			System.out.println("Number is the prime number");
+		    return true;
+	}    
+		else
+		{
+			System.out.println("Number is not prime number");
+			return false;
+		}
+	}
+
+
+    public static char getGrade(int score)
+	{
+		if(score>=60)
+		{
+			System.out.println("pass");
+			return true;
+		}
+		else
+		{
+			System.out.println("fail");
+			return false;
+		}
+	}
+	
+    public static boolean isEven(int number)
+	{	
+		if(number%2==0)
+		{
+			System.out.println("Number is even");
+		    return true;
+	}    
+		else
+		{
+			System.out.println("Number is not even");
+			return false;
+		}
+	}
+		
+    public static int addition(int x,int y)
+	{
+		int z=x+y;
+		if(z>0)
+		{
+			System.out.println("addition is valid");
+		    return true;
+		}
+		else
+		{
+			System.out.println("addition is not valid");
+		    return false;
+		}
+	}
+	
+	public static boolean checkStudentName(String name)
+	{
+		if(name.length() >= 5)
+		{
+			System.out.println("name is valid");
+			return true;
+		}
+		else
+		{
+			System.out.println("name not valid");
+			return false;
+		}
+	}
+	
+	public static long getLargeNumber(int num1,int num2)
+	{
+		if(num1>num2)
+		{
+			System.out.println("number is large");
+		    return num1;
+		}
+		else
+		{
+			System.out.println("number is not large");
+		    return num2;
+		}
+	}
+	
+	public static short isPositive(int number)
+	{	
+		if(number>0)
+		{
+			System.out.println("Number is positive");
+		    return true;
+	}    
+		else
+		{
+			System.out.println("Number is not positive");
+			return false;
+		}
+	}
+	
+	public static short isnegative(int number)
+	{	
+		if(number<0)
+		{
+			System.out.println("Number is negative");
+		    return number;
+	}    
+		else
+		{
+			System.out.println("Number is not negative");
+			return 0;
+		}
+	}
+	
+	public static long getSmallNumber(int number1,int number2)
+	{
+		if(number1<number2)
+		{
+			System.out.println("number is small");
+		    return number1;
+		}
+		else
+		{
+			System.out.println("number is not small");
+		    return number2;
+		}
+	}
--- methods.java
+++ methods.java
@@ -47,12 +47,10 @@ public class Methods{
 		}
 		public static void bytes()
 		{
-			byte [] tenTables = {10,20,30,40,50,60,70,80,90,100};
-			for(int i=0;i<tenTables.length;i++)
+			byte [] tenTable = {10,20,30,40,50,60,70,80,90,100};
+			for(int i=0;i<tenTable.length;i++)
 			{
-				System.out.println(tenTables[i]);
+				System.out.println(tenTable[i]);
 			}
-		}
-		
 	
 }
